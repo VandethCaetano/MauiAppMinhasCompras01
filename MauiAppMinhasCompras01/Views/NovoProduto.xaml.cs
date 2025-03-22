2031b10505b2adcf3828c2bc3e07763a3487307f
VandethCaetano <caetanovandeth@gmail.com>
2025-03-22 18:28:54 -0300

    atualização remover e editar

--- MauiAppMinhasCompras01/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras01/Views/NovoProduto.xaml.cs
@@ -13,8 +13,11 @@ public partial class NovoProduto : ContentPage
     {
 		try 
 		{
+			Produto produto_anexado = BindingContext as Produto;
+			
 			Produto p = new Produto 
 			{
+				Id = produto_anexado.Id,
 				Descricao = txt_descricao.Text,
 				Quantidade = Convert.ToDouble(txt_quantidade.Text),
 				Preco = Convert.ToDouble(txt_preco.Text)
